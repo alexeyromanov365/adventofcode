db11f3b9dcb424d715e38ac1d332149ec35bde74
Alexey Romanov <alexeyromanov365@gmail.com>
2017-04-17 16:10:55 +0300

    adventofcode day1 devided

---
+++ day1ver2/lib/coordinate.rb
@@ -0,0 +1,20 @@
+class Coordinate
+
+  def step_coordinate(direction, step)
+    case direction.to_s
+      when "North" then distance(Map.x, Map.y += step)
+      when "South" then distance(Map.x, Map.y -= step)
+      when "West" then distance(Map.x -= step, Map.y)
+      when "East" then distance(Map.x += step, Map.y)
+    end
+  end
+
+  private
+
+  def distance(x, y)
+    x.abs + y.abs
+  end
+
+end
+
+
---
+++ day1ver2/lib/file/file_reader.rb
@@ -0,0 +1,19 @@
+class FileReader
+
+  FILEPATH = 'file/key_string'.freeze
+
+  attr_reader :raw_moves, :readed_file
+
+  def initialize
+    @readed_file = File.read(FILEPATH)
+  end
+
+  def raw_moves
+    @raw_moves ||= readed_file.split(', ')
+  end
+
+  def moves
+  	@moves = raw_moves.map {|el| Move.new(el)}
+  end
+
+end
